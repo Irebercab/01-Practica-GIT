405f7e91f2cbb7d1f92e1785a2a2d8ac7ff6ae7e
Irene Beriain <irebercab@gmail.com>
2023-01-14 00:31:11 +0100

    Versión 0.5

--- TestMatematicas.java
+++ TestMatematicas.java
@@ -10,5 +10,22 @@ public class TestMatematicas {
 				System.out.println("El numero es par");
 			else
 				System.out.println("El número es impar");
+
+
+			boolean esDivisiblePorTres =
+					Matematicas.esDivisiblePorTres(numero);
+			if (esDivisiblePorTres)
+				System.out.println("El número introducido es divisible entre tres");
+			else
+				System.out.println("El número introducido no es divisible entre tres");
+
+
+			boolean esDivisiblePorCinco =
+					Matematicas.esDivisiblePorCinco(numero);
+
+			if (esDivisiblePorCinco)
+				System.out.println("El número introducido es divisible entre cinco");
+			else
+				System.out.println("El número introducido no es divisible entre cinco");
 		}
 	}
