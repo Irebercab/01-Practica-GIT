f561853f591105f56649d475ec97730706b0084b
Irene Beriain <irebercab@gmail.com>
2023-01-14 00:14:30 +0100

    Versión 0.3

--- Matematicas.java
+++ Matematicas.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 public class Matematicas {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int numero= sc.nextInt();
-		if (numero%2==0)
-		System.out.println("El número es par");
+		int numero = sc.nextInt();
+		if (numero % 2 == 0)
+			System.out.println("El número es par");
 		else
-		System.out.println("El número es impar");
+			System.out.println("El número es impar");
 	}
-		public static boolean esPar(int numero){
-			if (numero%2==0) return true;
-			else return false;
-		}
+
+	public static boolean esPar(int numero) {
+		if (numero % 2 == 0) return true;
+		else return false;
 	}
+
+
+	public static boolean esDivisiblePorTres(int numero) {
+		if (numero % 3 == 0)
+			return true;
+		return false;
+	}
+
+	public static boolean esDivisiblePorCinco(int numero) {
+		if (numero % 5 == 0)
+			return true;
+		return false;
+	}
+}
